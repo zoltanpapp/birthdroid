8d5ad2491d2af4230400038661e1647252e7f37c
thowi <thomas@wittek.me>
2014-06-12 10:51:30 +0200

    model

---
+++ project/src/com/thozo/birthdroid/model/Birthdays.java
@@ -0,0 +1,7 @@
+package com.thozo.birthdroid.model;
+
+import java.util.Collection;
+
+public class Birthdays {
+	public Collection<Person> people;
+}
---
+++ project/src/com/thozo/birthdroid/model/Person.java
@@ -0,0 +1,8 @@
+package com.thozo.birthdroid.model;
+
+import java.util.Date;
+
+public class Person {
+	public String name;
+	public Date birthday;
+}
