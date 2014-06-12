b89d0b88752c77e32901caeb97985e4d54783d05
thowi <thomas@wittek.me>
2014-06-12 16:30:51 +0200

    Cleanup

--- project/src/com/thozo/birthdroid/EditBirthdayActivity.java
+++ project/src/com/thozo/birthdroid/EditBirthdayActivity.java
@@ -12,17 +12,14 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.DatePicker;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.thozo.birthdroid.model.Person;
 
@@ -107,7 +104,7 @@ public class EditBirthdayActivity extends Activity {
 		}
 		
 		public void onDateSet(DatePicker view, int year, int month, int day) {
-			birthdayTextView.setText(month + "-" + day);
+			birthdayTextView.setText((month + 1) + "-" + day);
 			// TODO(wittek): Set on Model.
 		}
 		
