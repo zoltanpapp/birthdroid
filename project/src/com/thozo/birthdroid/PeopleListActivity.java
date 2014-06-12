8ab1f15ba3b5348ce6989efae7ac66cd511df7e0
Nikolay Zherebtsov <evs@evs-macbookair.roam.corp.google.com>
2014-06-12 11:30:54 +0200

    Adding Edit activity.

--- project/src/com/thozo/birthdroid/PeopleListActivity.java
+++ project/src/com/thozo/birthdroid/PeopleListActivity.java
@@ -1,15 +1,34 @@
 package com.thozo.birthdroid;
 
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.text.format.DateFormat;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PeopleListActivity extends Activity {
+
+	private TextView currentDateView;
+	private Button addBirthdayButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_people_list);
+		
+		currentDateView = (TextView) findViewById(R.id.currentDateView);
+		currentDateView.setText("Current date: " + DateFormat.format("yyyy-MM-dd", new Date()));
+
+		addBirthdayButton = (Button) findViewById(R.id.addBirthdayButton);
+	}
+	
+	public void handleAddBirthdayButtonClick() {
+		Intent intent = new Intent(this, EditBirthday.class);
+		startActivity(intent);
 	}
 
 	@Override
