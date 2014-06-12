961796c8526eb4b5d6a4dbb2970fc69f99fc5558
thowi <thomas@wittek.me>
2014-06-12 15:39:18 +0200

    Add a photo picker to the edit fragment.

--- project/src/com/thozo/birthdroid/EditBirthdayActivity.java
+++ project/src/com/thozo/birthdroid/EditBirthdayActivity.java
@@ -1,15 +1,19 @@
 package com.thozo.birthdroid;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 public class EditBirthdayActivity extends Activity {
 
@@ -28,6 +32,8 @@ public class EditBirthdayActivity extends Activity {
 	 * A placeholder fragment containing a simple view.
 	 */
 	public static class PlaceholderFragment extends Fragment {
+		private static final int SELECT_PHOTO = 100;
+		private ImageView photoImageView;
 
 		public PlaceholderFragment() {
 		}
@@ -37,16 +43,39 @@ public class EditBirthdayActivity extends Activity {
 				Bundle savedInstanceState) {
 			View rootView = inflater.inflate(R.layout.fragment_edit_birthday,
 					container, false);
-			
-			ImageView photoImageView = (ImageView) rootView.findViewById(R.id.photoImageView);
+
+			photoImageView = (ImageView) rootView.findViewById(R.id.photoImageView);
 			photoImageView.setOnClickListener(new View.OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					Toast.makeText(getActivity(), "Select photo!", Toast.LENGTH_LONG).show();
+					Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+					photoPickerIntent.setType("image/*");
+					startActivityForResult(photoPickerIntent, SELECT_PHOTO);
 				}
 			});
-			
+
 			return rootView;
+		}
+
+		@Override
+		public void onActivityResult(int requestCode, int resultCode,
+				Intent imageReturnedIntent) {
+			super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+			switch (requestCode) {
+			case SELECT_PHOTO:
+				if (resultCode == RESULT_OK) {
+					Uri selectedImage = imageReturnedIntent.getData();
+					try {
+						InputStream imageStream = getActivity()
+								.getContentResolver()
+								.openInputStream(selectedImage);
+						Bitmap bitmap = BitmapFactory.decodeStream(imageStream);
+						photoImageView.setImageBitmap(bitmap);
+					} catch (FileNotFoundException e) {
+					}
+				}
+			}
 		}
 	}
 
